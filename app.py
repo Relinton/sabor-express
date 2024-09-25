ee45b2aa3032a5f7fbcfb2da1cdd3b1d30679d4c
Relinton Pinheiro <relintonproande@gmail.com>
2024-09-25 19:58:02 -0300

    sabor-express - lista, laços e exceções

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Restaurante que delícia', 'Restaurante aconchego', 'Restaurante mata fome']
+
 def exibir_nome_do_programa():
     print("""
 
@@ -12,53 +14,66 @@ def exibir_nome_do_programa():
       """)
     
 def exibir_opcoes():
-    print('1. Cadastrar Restaurante')
-    print('2. Listar Restaurantes')
-    print('3. Ativar Restaurante')
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
+    exibir_subtitulo('Finalizando app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
     os.system('cls')
-    print('Finalizando o app\n')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    #print(f'Você escolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
 
-    if opcao_escolhida == 1:
-        print('Cadastrar Restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar Restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1: 
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else: 
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-# print('Python na Escolha de Programação da Alura.')
-
-# nome = 'Relinton'
-# idade = 30
-# print(f'Meu nome é {nome} e tenho {idade} anos')
-
-# print('A\n')
-# print('L\n')
-# print('U\n')
-# print('R\n')
-# print('A')
-
-# pi_arredondado = 3.14159
-# print(f'O valor arredondado de pi é: {pi_arredondado:.2f}')
